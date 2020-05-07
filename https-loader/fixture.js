c649d53a2e09b7c9cb59d4232f668b61e105fc13
Geoffrey Booth <webmaster@geoffreybooth.com>
2020-05-07 12:52:23 -0700

    Add test for https loader

---
+++ https-loader/fixture.js
@@ -0,0 +1,3 @@
+import { VERSION } from 'https://coffeescript.org/browser-compiler-modern/coffeescript.js';
+
+console.log(`The browser-based version of CoffeeScript hosted at coffeescript.org is: ${VERSION}`);
--- https-loader/test.js
+++ https-loader/test.js
@@ -1,3 +1,23 @@
-import { VERSION } from 'https://coffeescript.org/browser-compiler-modern/coffeescript.js';
+import { ok } from 'assert';
+import { spawn } from 'child_process';
+import { execPath } from 'process';
 
-console.log(`The browser-based version of CoffeeScript hosted at coffeescript.org is: ${VERSION}`);
+
+// Run this test yourself with debugging mode via:
+// node --inspect-brk --experimental-loader ./loader.js ./fixture.js
+
+const child = spawn(execPath, [
+  '--experimental-loader',
+  './loader.js',
+  './fixture.js'
+]);
+
+let stdout = '';
+child.stdout.setEncoding('utf8');
+child.stdout.on('data', (data) => {
+  stdout += data;
+});
+
+child.on('close', (code, signal) => {
+  ok(/The browser-based version of CoffeeScript hosted at coffeescript\.org is: \d+\.\d+\.\d+/.test(stdout.toString()));
+});

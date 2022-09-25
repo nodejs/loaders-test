ed9a48ee2823816916f429fbce9553f2e8d3dad4
Geoffrey Booth <webadmin@geoffreybooth.com>
2022-09-24 22:08:08 -0700

    Update loader to throw same error code as Node for module not found; follow experimental-specifier-resolution behavior of searching for .mjs extensions in addition to legacy CommonJS ones

--- commonjs-extension-resolution-loader/loader.js
+++ commonjs-extension-resolution-loader/loader.js
@@ -24,11 +24,22 @@ export async function resolve(specifier, context, next) {
   }
   const parentPath = fileURLToPath(parentURL);
 
-  const resolution = await resolveAsync(specifier, {
-    basedir: dirname(parentPath),
-    extensions: ['.js', '.json', '.node'],
-  });
-  const url = pathToFileURL(resolution).href;
+  let url;
+  try {
+    const resolution = await resolveAsync(specifier, {
+      basedir: dirname(parentPath),
+      // For whatever reason, --experimental-specifier-resolution=node doesn't search for .mjs extensions
+      // but it does search for index.mjs files within directories
+      extensions: ['.js', '.json', '.node', '.mjs'],
+    });
+    url = pathToFileURL(resolution).href;
+  } catch (error) {
+    if (error.code === 'MODULE_NOT_FOUND') {
+      // Match Node's error code
+      error.code = 'ERR_MODULE_NOT_FOUND';
+    }
+    throw error;
+  }
 
   return next(url, context);
 }
